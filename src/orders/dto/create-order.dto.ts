56452e0e082ca00174683ebafcec5e5de2a16ddd
Jim Loza <jimloza25@gmail.com>
2024-08-31 11:41:23 -0600

    feat(database): add database config and create order

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,1 +1,21 @@
-export class CreateOrderDto {}
+import { OrderStatus } from "@prisma/client";
+import { IsBoolean, IsEnum, IsNumber, IsOptional, IsPositive } from "class-validator";
+import { OrderSatusList } from "../enum/order.enum";
+
+
+export class CreateOrderDto {
+    @IsNumber()
+    @IsPositive()
+    totalAmount: number;
+    @IsNumber()
+    @IsPositive()
+    totalItems: number;
+    @IsEnum(OrderSatusList, {
+        message: `Possible values are ${OrderSatusList.join(', ')}`
+    })
+    @IsOptional()
+    status: OrderStatus = OrderStatus.PENDING;
+    @IsBoolean()
+    @IsOptional()
+    paid: boolean = false;
+}
